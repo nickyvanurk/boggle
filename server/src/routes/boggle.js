e23ff6e224356f95a8d6f5fb01e61143e61d7661
Nicky van Urk <nickyvanurk@gmx.com>
2020-04-20 12:14:55 +0200

    Add boggle api endpoints

--- server/src/routes/boggle.js
+++ server/src/routes/boggle.js
@@ -2,8 +2,30 @@ import express from 'express';
 
 const router = new express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello World!');
+router.get('/getboggleboard', ({ query }, res) => {
+  if (!query.id) {
+    // return random board with random seed
+  } else {
+    // return random boggle board with given seed (id)
+  };
+
+  res.send('getboggleboard');
+});
+
+router.get('/isvalidword', ({ query }, res) => {
+  if (!query.id || !query.word) {
+    res.send({ error: 'You must provide a board id and word' });
+  }
+
+  res.send('isvalidword');
+});
+
+router.get('/scoreword', ({ query }, res) => {
+  if (!query.word) {
+    res.send({ error: 'You must provide a word' });
+  }
+
+  res.send('scoreword');
 });
 
 export default router;
