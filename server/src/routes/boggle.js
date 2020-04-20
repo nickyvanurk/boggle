9150c383bf7479fa12e6f82813fed858e563b297
Nicky van Urk <nickyvanurk@gmx.com>
2020-04-20 12:43:51 +0200

    Implement getboggleboard endpoint

--- server/src/routes/boggle.js
+++ server/src/routes/boggle.js
@@ -1,15 +1,29 @@
 import express from 'express';
+import Boggle from '../utils/boggle';
+import validator from 'validator';
 
 const router = new express.Router();
 
 router.get('/getboggleboard', ({ query }, res) => {
   if (!query.id) {
-    // return random board with random seed
-  } else {
-    // return random boggle board with given seed (id)
-  };
+    const id = Boggle.getRandomId();
 
-  res.send('getboggleboard');
+    return res.send({
+      id: id,
+      board: Boggle.getRandomBoard(id)
+    });
+  }
+
+  if (!validator.isInt(query.id)) {
+    return res.send({
+      error: 'Invalid id'
+    });
+  }
+
+  res.send({
+    id: query.id,
+    board: Boggle.getRandomBoard(query.id)
+  });
 });
 
 router.get('/isvalidword', ({ query }, res) => {
---
+++ server/src/utils/boggle.js
@@ -0,0 +1,38 @@
+import { randomNumberGenerator } from './math';
+
+export default class Boggle {
+  static dice = [
+    ['R', 'I', 'F', 'O', 'B', 'X'],
+    ['I', 'F', 'E', 'H', 'E', 'Y'],
+    ['D', 'E', 'N', 'O', 'W', 'S'],
+    ['U', 'T', 'O', 'K', 'N', 'D'],
+    ['H', 'M', 'S', 'R', 'A', 'O'],
+    ['L', 'U', 'P', 'E', 'T', 'S'],
+    ['A', 'C', 'I', 'T', 'O', 'A'],
+    ['Y', 'L', 'G', 'K', 'U', 'E'],
+    ['Q', 'B', 'M', 'J', 'O', 'A'],
+    ['E', 'H', 'I', 'S', 'P', 'N'],
+    ['V', 'E', 'T', 'I', 'G', 'N'],
+    ['B', 'A', 'L', 'I', 'Y', 'T'],
+    ['E', 'Z', 'A', 'V', 'N', 'D'],
+    ['R', 'A', 'L', 'E', 'S', 'C'],
+    ['U', 'W', 'I', 'L', 'R', 'G'],
+    ['P', 'A', 'C', 'E', 'M', 'D']
+  ];
+
+  static getRandomId() {
+    return Math.floor(Math.random() * 1000000000001);
+  }
+
+  static getRandomBoard(seed) {
+    const board = [];
+    const rng = seed ? randomNumberGenerator(seed) : Math.random;
+
+    for (const die of this.dice) {
+      const randomDieValue = die[Math.floor(rng() * Math.floor(6))];
+      board.push(randomDieValue);
+    }
+
+    return board;
+  }
+}
