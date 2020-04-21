fea225afee74775e92828bd289f782ae522ba966
Nicky van Urk <nickyvanurk@gmx.com>
2020-04-21 22:23:48 +0200

    Refactor square selection

--- client/src/components/board.js
+++ client/src/components/board.js
@@ -5,30 +5,9 @@ export default class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: null,
-      squares: Array(16).fill(null),
       selection: [],
       isSelecting: false,
-      error: null,
-      isLoaded: false
     };
-  }
-
-  componentDidMount() {
-    fetch('//localhost:3000/getboggleboard')
-      .then(res => res.json())
-      .then((result) => {
-        this.setState({
-          isLoaded: true,
-          id: result.id,
-          squares: result.board
-        });
-      }, (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      });
   }
 
   handleMouseDown(i) {
@@ -50,8 +29,8 @@ export default class Board extends React.Component {
   }
 
   handleMouseUp() {
-    const {id, selection} = this.state;
-    this.checkSelection(id, selection);
+    const {selection} = this.state;
+    this.props.onSelection(selection);
     this.resetSelection();
   }
 
@@ -60,8 +39,8 @@ export default class Board extends React.Component {
       return;
     }
 
-    const {id, selection} = this.state;
-    this.checkSelection(id, selection);
+    const {selection} = this.state;
+    this.props.onSelection(selection);
     this.resetSelection();
   }
 
@@ -78,21 +57,9 @@ export default class Board extends React.Component {
     });
   }
 
-  checkSelection(id, selection) {
-    fetch(`//localhost:3000/isvalidword?id=${id}&selection=${selection}`)
-      .then(res => res.json())
-      .then(({valid, word}) => {
-        if (valid) {
-          this.props.onFoundWord(word);
-        }
-      }, (error) => {
-        this.setState({error});
-      });
-  }
-
   renderSquare(i) {
     return (
-      <Square value={this.state.squares[i]}
+      <Square value={this.props.squares[i]}
               isSelected={this.state.selection.includes(i)}
               onMouseDown={() => this.handleMouseDown(i)}
               onMouseEnter={() => this.handleMouseEnter(i)} />
@@ -100,42 +67,34 @@ export default class Board extends React.Component {
   }
 
   render() {
-    const {error, isLoaded} = this.state;
-
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <div onMouseUp={() => this.handleMouseUp()}
-             onMouseLeave={() => this.handleMouseLeave()}>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            {this.renderSquare(3)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(8)}
-            {this.renderSquare(9)}
-            {this.renderSquare(10)}
-            {this.renderSquare(11)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(12)}
-            {this.renderSquare(13)}
-            {this.renderSquare(14)}
-            {this.renderSquare(15)}
-          </div>
+    return (
+      <div onMouseUp={() => this.handleMouseUp()}
+           onMouseLeave={() => this.handleMouseLeave()}>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
         </div>
-      );
-    }
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+          {this.renderSquare(10)}
+          {this.renderSquare(11)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(12)}
+          {this.renderSquare(13)}
+          {this.renderSquare(14)}
+          {this.renderSquare(15)}
+        </div>
+      </div>
+    );
   }
 }
--- client/src/components/game.js
+++ client/src/components/game.js
@@ -5,24 +5,63 @@ class Game extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      foundWords: []
+      id: null,
+      squares: Array(16).fill(null),
+      foundWords: [],
+      isLoaded: false,
+      error: null
     };
   }
 
-  handleFoundWord(word) {
-    const foundWords = this.state.foundWords.slice();
-    foundWords.push(word);
-    this.setState({foundWords});
+  componentDidMount() {
+    fetch('//localhost:3000/getboggleboard')
+      .then(res => res.json())
+      .then((result) => {
+        this.setState({
+          isLoaded: true,
+          id: result.id,
+          squares: result.board
+        });
+      }, (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      });
+  }
+
+  handleSelection(selection) {
+    const {id} = this.state;
+    fetch(`//localhost:3000/isvalidword?id=${id}&selection=${selection}`)
+      .then(res => res.json())
+      .then(({valid, word}) => {
+        if (valid) {
+          const foundWords = this.state.foundWords.slice();
+          foundWords.push(word);
+          this.setState({foundWords});
+        }
+      }, (error) => {
+        this.setState({error});
+      });
   }
 
   render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board onFoundWord={(word) => this.handleFoundWord(word)} />
+    const {error, isLoaded} = this.state;
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board squares={this.state.squares}
+                  onSelection={(selection) => this.handleSelection(selection)} />
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
