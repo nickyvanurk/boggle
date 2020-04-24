5a3bc230e92099d6dc0c1c587d79180784619a4f
Nicky van Urk <nickyvanurk@gmx.com>
2020-04-24 12:05:10 +0200

    Add form validation

--- client/src/components/game-over.js
+++ client/src/components/game-over.js
@@ -21,18 +21,28 @@ export default class GameOver extends React.Component {
     this.props.onHighscoreSubmit(this.state.playerName, this.props.totalScore);
   }
 
+  validateInput(playerName) {
+    return {
+      playerName: playerName.length < 3 || playerName.length >= 16
+    };
+  }
+
   render() {
     const {totalScore} = this.props;
+    const errors = this.validateInput(this.state.playerName);
+    const isEnabled = !Object.keys(errors).some(x => errors[x]);
 
     return (
       <div>
         <h2>Game Over</h2>
         <span>You scored {totalScore} {totalScore === 1 ? 'point' : 'points'}</span>
         <form onSubmit={this.handleSubmit}>
-          <input placeholder="Player name"
+          <input className={this.state.playerName.length > 0 && errors.playerName ? 'error' : ''}
+                 placeholder="Player name"
                  value={this.state.value}
                  onChange={this.handleChange} />
-          <button type="submit">Submit Highscore</button>
+          <button type="submit"
+                  disabled={!isEnabled}>Submit Highscore</button>
         </form>
       </div>
     );
